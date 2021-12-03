cce15c6695d5da0e2ca6b4df9f0be87b54fb5574
Matej Cubek <matej.cubek@neos.hr>
2021-12-03 11:59:34 +0100

    lab2 project init

---
+++ Lab2/lab2-controller/src/main/java/hr/fer/rassus/lab2/lab2controller/Lab2ControllerApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.lab2.lab2controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2ControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2ControllerApplication.class, args);
+    }
+
+}
---
+++ Lab2/lab2-node/src/main/java/hr/fer/rassus/lab2/lab2node/Lab2NodeApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.lab2.lab2node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2NodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2NodeApplication.class, args);
+    }
+
+}
