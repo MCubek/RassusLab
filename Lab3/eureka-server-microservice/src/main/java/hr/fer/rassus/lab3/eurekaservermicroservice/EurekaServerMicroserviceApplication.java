e8f501ef62e413139eb2a4c13a2d95115d41bfab
MCubek <matej.cubek@gmail.com>
2021-12-29 15:06:24 +0100

    Lab3 init

---
+++ Lab3/eureka-server-microservice/src/main/java/hr/fer/rassus/lab3/eurekaservermicroservice/EurekaServerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.lab3.eurekaservermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Lab3/eureka-server-microservice/src/test/java/hr/fer/rassus/lab3/eurekaservermicroservice/EurekaServerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.lab3.eurekaservermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
