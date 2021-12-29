81566bcb79b0de56770245dafa7d599922582760
MCubek <matej.cubek@gmail.com>
2021-12-29 16:16:58 +0100

    Added some configs

--- Lab3/config-server-microservice/src/main/java/hr/fer/rassus/lab3/configservermicroservice/ConfigServerMicroserviceApplication.java
+++ Lab3/config-server-microservice/src/main/java/hr/fer/rassus/lab3/configservermicroservice/ConfigServerMicroserviceApplication.java
@@ -2,8 +2,10 @@ package hr.fer.rassus.lab3.configservermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerMicroserviceApplication {
 
     public static void main(String[] args) {
